6887756ef5b2b69f68f3bee090de895fcc9f7f8e
imoghul <immoghul@grendel33.ece.ncsu.edu>
2023-10-14 18:10:53 -0400

    HW6 working

--- HW6/rtl/dut.sv
+++ HW6/rtl/dut.sv
@@ -11,7 +11,7 @@ module MyDesign (
     //---------------------------------------------------------------------------
     //Control signals
     input  wire dut_valid,
-    output reg dut_ready,
+    output reg  dut_ready,
 
     //---------------------------------------------------------------------------
     //q_state_output SRAM interface
@@ -25,16 +25,16 @@ module MyDesign (
 
   //---------------------------------------------------------------------------
   //q_state_output SRAM interface
-  reg              sram_write_enable_r;
+  reg         sram_write_enable_r;
   // reg       [31:0] sram_write_data_r;
-  reg       [15:0] sram_address;
-  reg              compute_complete;
-  reg [31:0] addr_count;
-  reg[1:0] load_count;
-  reg[1:0] start_addr_count;
-  reg start_accum;
+  reg  [15:0] sram_address;
+  reg         compute_complete;
+  reg  [31:0] addr_count;
+  reg  [ 1:0] load_count;
+  reg  [ 1:0] start_addr_count;
+  reg         start_accum;
 
-  
+
   wire [31:0] sum_calculation;
   reg  [31:0] sum;
   // reg  [31:0] in;
@@ -49,7 +49,7 @@ module MyDesign (
   // param list for the DW_fp_add, you will only need one DW_fp_add
 
   // synopsys translate_off
-  shortreal        test_val;
+  shortreal test_val;
   assign test_val = $bitstoshortreal(sum);
   // This is a helper val for seeing the 32bit flaot value, you can repicate 
   // this for any signal, but keep it between the translate_off and
@@ -62,58 +62,61 @@ module MyDesign (
   localparam INITIALIZE_COUNT = 1;
   localparam ACCUMULATE = 2;
   localparam WRITE = 3;
-  reg[1:0] state, nextState;
+  localparam RESET = 4;
+  reg [2:0] state, nextState;
 
-  always @ (posedge clk) begin
-    if(!reset_n) state <= IDLE;
-    else state<=nextState;
+  always @(posedge clk) begin
+    if (!reset_n) state <= IDLE;
+    else state <= nextState;
   end
 
 
-  always @(*)begin
+  always @(*) begin
     start_accum = 0;
     start_addr_count = 0;
     load_count = 2;
     dut_ready = 1;
     sram_write_enable_r = 0;
-    case(state)
-      IDLE:begin
-        if(dut_valid) begin
+    case (state)
+      IDLE: begin
+        if (dut_valid) begin
           sram_write_enable_r = 0;
           start_addr_count = 1;
           nextState = INITIALIZE_COUNT;
-        end
-        else begin
+        end else begin
           nextState = IDLE;
         end
       end
-      INITIALIZE_COUNT:begin
+      INITIALIZE_COUNT: begin
         nextState = ACCUMULATE;
         load_count = 1;
         start_addr_count = 1;
         sram_write_enable_r = 0;
         dut_ready = 0;
       end
-      ACCUMULATE:begin
+      ACCUMULATE: begin
         load_count = 0;
         start_accum = 1;
         sram_write_enable_r = 0;
         start_addr_count = 1;
         dut_ready = 0;
-        if(addr_count == 0) nextState = IDLE;
-        else if(addr_count == 1) begin
+        if (addr_count == 0) nextState = IDLE;
+        else if (addr_count == 1) begin
           start_addr_count = 2;
           nextState = WRITE;
-        end
-        else begin
+        end else begin
           nextState = ACCUMULATE;
         end
       end
-      WRITE:begin
+      WRITE: begin
         start_addr_count = 0;
         dut_ready = 0;
         sram_write_enable_r = 1;
         start_accum = 0;
+        nextState = RESET;
+      end
+      RESET: begin
+        dut_ready = 0;
         nextState = IDLE;
       end
       default: nextState = IDLE;
@@ -122,30 +125,30 @@ module MyDesign (
 
 
   // datapath
-  
+
   always @(posedge clk) begin
     if (!reset_n) addr_count <= 0;
     else begin
-      if(load_count == 0) addr_count<=addr_count - 1;
-      else if(load_count == 1) addr_count<=sram_read_data;
+      if (load_count == 0) addr_count <= addr_count - 1;
+      else if (load_count == 1) addr_count <= sram_read_data;
       else if (load_count == 2) addr_count <= addr_count;
     end
   end
 
 
   always @(posedge clk) begin
-    if(!reset_n) sram_address <= 0;
+    if (!reset_n) sram_address <= 0;
     else begin
-      if(start_addr_count == 0) sram_address<=0;
-      else if(start_addr_count == 1) sram_address<=sram_address+1;
+      if (start_addr_count == 0) sram_address <= 0;
+      else if (start_addr_count == 1) sram_address <= sram_address + 1;
       else if (start_addr_count == 2) sram_address <= sram_address;
     end
   end
 
-  always @(posedge clk)begin
-    if(!reset_n) sum<=32'b0;
+  always @(posedge clk) begin
+    if (!reset_n) sum <= 32'b0;
     else begin
-      sum<=start_accum?sum_calculation:0;
+      sum <= start_accum ? sum_calculation : 0;
     end
   end
 
